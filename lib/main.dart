465a26ce7dce2c83fbb69c1159d36f8cbcc5c584
Stephen Janseen Balo <66478367+pure-edge@users.noreply.github.com>
2025-08-12 16:41:26 +0800

    Update main.dart
    imported new class Computer, and create a Computer object

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,6 @@ void main() {
   var person = Person("Stephen", 18);
   person.sayHello();
 
-  var computer = Computer("ASUS", "ASUS Vivobook");
+  var computer = Computer("Acer", "Acer Ambot brand");
   computer.describe();
 }
